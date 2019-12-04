0e4ada7e0b5adb388e2385b4b7f4f1214c849b1f
Svan1233 <55134376+Svan1233@users.noreply.github.com>
2019-12-04 21:10:38 +0100

    Add files via upload

---
+++ svanTipuricZadacaTutorial2/svanTipuricZadacaTutorial2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace svanTipuricZadacaTutorial2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("The number could not be converted to byte.");
+            }
+
+
+            
+        }
+    }
+}
